65827db71c4bc5f33c08cde78225d66b4512a7f5
Andrei Kopysov <yaskpp@gmail.com>
2015-08-27 11:00:08 +0500

    Codec sample

---
+++ ranch_test/priv/RanchDemo/Assets/Codecs/ChatClient.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Text;
+
+namespace yaski.codecs.chat {
+
+	public partial class ChatClient {
+
+		// To Server
+		private void Connect()
+		{
+			Send (this, 3);
+		}
+
+		private void SendMessage(string message)
+		{
+			var data = Encoding.UTF8.GetBytes(message);
+			Send (this, 6, data, data.Length);
+		}
+
+		private void Close()
+		{
+			Send (this, 2);
+		}
+
+		// -===== From Server =====-
+
+		public struct Type0
+		{
+			byte s0;
+			byte s1;
+			byte s2;
+		}
+		partial void OnInit(Type0 arg);
+
+		public struct Type1
+		{
+			long time;
+			byte user;
+			string message;
+		}
+		partial void OnReceiveMessage(Type1 arg);
+
+		// Internal implementation
+
+		public static void Call(ChatClient client, byte[] data, int length)
+		{
+
+		}
+
+		public static void Send(ChatClient client, byte type)
+		{
+
+		}
+
+		public static void Send(ChatClient client, byte type, byte[] data, int length)
+		{
+			
+		}
+
+	}
+
+}
---
+++ ranch_test/priv/RanchDemo/Assets/Scripts/ChatClient.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace yaski.codecs.chat {
+
+	public partial class ChatClient : MonoBehaviour {
+
+		partial void OnInit(Type0 arg)
+		{
+			Debug.Log("On Init");
+		}
+		
+		partial void OnReceiveMessage(Type1 arg)
+		{
+			Debug.Log("On Message");
+		}
+
+	}
+
+}
