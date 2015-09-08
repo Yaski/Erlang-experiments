69c51fb131436ad8758f5406e486dea9fb9434da
Andrei Kopysov <yaskpp@gmail.com>
2015-09-08 11:07:47 +0500

    Custom filters

--- binary_codec_test/src/codec_generator_erlydtl_library.erl
+++ binary_codec_test/src/codec_generator_erlydtl_library.erl
@@ -1,18 +1,30 @@
+-module(codec_generator_erlydtl_library).
 %% API
 -behaviour(erlydtl_library).
 -export([inventory/1, version/0]).
 
--export([filter_packets_by_params/2]).
+-export([filter_packets/2, pascal_case/1, snake_case/1]).
 
 version() -> 1.
 
 inventory(tags) -> [];
 inventory(filters) ->
-  [filter_packets_by_params].
+  [filter_packets, pascal_case, snake_case].
 
-filter_packets_by_params(Value, Params) ->
+filter_packets(Value, Params) ->
   lists:filter(
     fun (Packet) ->
       proplists:get_value(params, Packet) == Params end,
     Value
   ).
+
+pascal_case(Value) ->
+  Str = string:tokens(Value, "_"),
+  Val = [[string:to_upper(X) | Y] || [X | Y] <- Str],
+  string:join(Val, "").
+
+snake_case(Value) when is_binary(Value) ->
+  string:to_lower(binary_to_list(Value));
+
+snake_case(Value) when is_list(Value) ->
+  string:to_lower(Value).
